9dc3d627c0bd44464f0861455ff5178793c09de5
amanoooo <1060996790@qq.com>
2022-12-26 18:21:28 +0800

    finish test

---
+++ client.ts
@@ -0,0 +1,106 @@
+import { generate } from 'qrcode-terminal'
+import { Contact, Message, WechatyBuilder } from 'wechaty'
+import axios from 'axios'
+
+
+
+
+const wechaty = WechatyBuilder.build() // get a Wechaty instance
+
+// async function onScan(qrcode: string, status: number) {
+//   generate(qrcode, { small: true })
+
+//   // Generate a QR Code online via
+//   // http://goqr.me/api/doc/create-qr-code/
+//   const qrcodeImageUrl = [
+//     'https://api.qrserver.com/v1/create-qr-code/?data=',
+//     encodeURIComponent(qrcode),
+//   ].join('')
+
+//   console.log(`[${status}] ${qrcodeImageUrl}\nScan QR Code above to log in: `)
+// }
+async function onMessage(msg: Message) {
+  console.log(msg.toString())
+
+  const response: any = await tryGetResponse(msg)
+  if (response.code === 0) {
+    msg.say(response.data)
+    return
+ } else {
+   console.log('ignore response');
+   return
+ }
+
+  // if (msg.age() > 60) {
+  //   console.log('Message discarded because its TOO OLD(than 1 minute)')
+  //   return
+  // }
+
+  // if (msg.type() !== wechaty.Message.Type.Text
+  //   || !/^(ding|ping|bing|code)$/i.test(msg.text())
+  //   /*&& !msg.self()*/
+  // ) {
+    
+    
+  //   return
+  // }
+
+  /**
+   * 1. reply 'dong'
+   */
+  // await msg.say('dong')
+  // console.log('REPLY: dong')
+
+  // reference https://github.com/wechaty/wechaty/blob/1523c5e02be46ebe2cc172a744b2fbe53351540e/examples/ding-dong-bot.ts
+
+}
+
+
+async function tryGetResponse(message: Message) {
+  console.log(`Message: ${message}`)
+  const response = await axios({
+    method: 'post',
+    url: 'http://127.0.0.1:3000/message',
+    data: {
+      message
+    }
+  });
+  console.log('receive from cus server with data %o', response.data);
+  return response.data
+}
+
+async function handleAction(message: Message) {
+
+}
+
+function onError(e: Error) {
+  console.error('Bot error:', e)
+  /*
+  if (bot.logonoff()) {
+    bot.say('Wechaty error: ' + e.message).catch(console.error)
+  }
+  */
+}
+
+function onLogout(user: Contact) {
+  console.log(`${user.name()} logouted`)
+}
+
+
+async function main() {
+  wechaty
+    .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`))
+    // .on('scan', (qrcode: string, status: number) => onScan(qrcode, status))
+    .on('login', (user: Contact) => console.log(`User ${user} logged in`))
+    .on('message', onMessage)
+    .on('error', onError)
+    .on('logout', onLogout)
+  wechaty.start()
+
+}
+
+
+// @ts-ignore
+// tryGetResponse({a: 1})
+
+main()
--- server.js
+++ server.js
@@ -1,8 +1,77 @@
 const Koa = require('koa');
-const app = new Koa();
+const Router = require('@koa/router');
+const { koaBody } = require('koa-body');
 
-app.use(async ctx => {
-  ctx.body = 'Hello World';
+
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+    // ctx.router available
 });
+
+
+const ME = '@48e818fe15472f801adf9ff42625a1dcaabbedb1447595af0e49658d67e390de'
+const MINMIN = '@bd60b772c2d973375c77721fd4f9c6a3209c8669ca20f29584f994000f587b4f'
+const TEST_ROMM_ID = '@@955a0636df1faaaefb23f06d3dedea4c4513971063d89d8c3952ba3e36103400'
+/**
+ * message: {
+    _events: {},
+    _eventsCount: 0,
+    id: '2535429498782572035',
+    payload: {
+      id: '2535429498782572035',
+      talkerId: '@48e818fe15472f801adf9ff42625a1dcaabbedb1447595af0e49658d67e390de',
+      text: 'ding',
+      timestamp: 1672048191,
+      type: 7,
+      listenerId: 'filehelper'
+    }
+  }
+ */
+router.post('/message', (ctx, next) => {
+    console.log('receive /message %o', ctx.request.body);
+    const body = ctx.request.body
+
+    const payload = body.message.payload
+    // 敏敏发给我的
+    if (payload?.talkerId === MINMIN
+        && payload?.listenerId === ME) {
+        if (payload?.text === 'ding') {
+            console.log('receive ding')
+            return ctx.body = {
+                code: 0,
+                data: 'ok in pair',
+            }
+
+        } else {
+            console.log('warn ignore')
+        }
+        // Wechat bot 群
+    } else if (payload.roomId === TEST_ROMM_ID) {
+        if (payload.text === '@amanoooo ding') {
+            return ctx.body = {
+                code: 0,
+                data: 'ok in room',
+            }
+        } else {
+            console.log('warn ignore with room')
+        }
+    }
+    return ctx.body = {
+        code: 1,
+        data: 'not ok',
+    }
+});
+
+
+app
+    .use(koaBody())
+    .use(router.routes())
+    .use(router.allowedMethods())
+
+
+
 
 app.listen(3000);
